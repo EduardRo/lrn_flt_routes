df8239b670c293436de83cb0d43bc1240c91ac2c
EduardRo <eduardstancu@yahoo.com>
2023-01-27 21:25:05 +0200

    modif 01012723

---
+++ lib/route.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:lrn_flt_routes/home_page.dart';
+
+// Routes Names
+class RouteGenerator {
+// Control our page route flow
+
+  static Route<dynamic> controllerRoute(RouteSettings settings) {
+    //final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (context) => const HomePage());
+
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('error'),
+        ),
+        body: const Center(
+          child: Text('error'),
+        ),
+      );
+    });
+  }
+}
